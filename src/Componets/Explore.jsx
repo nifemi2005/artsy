71290c62089c669a5e742bc59e4c742b81b61744
Oluwanifemi <temenuoluwanifemi2005@gmail.com>
2023-01-10 17:30:23 +0100

    Styled the Mobile version of the Explore componet

--- src/Componets/Explore.jsx
+++ src/Componets/Explore.jsx
@@ -3,23 +3,23 @@ import { Link } from 'react-router-dom'
 
 const Explore = () => {
   return (
-    <div className='lg:py-48'>
+    <div className='lg:py-48 md:py-32 sm:py-20 py-10'>
         <hr />
-        <div className='flex items-center justify-between lg:px-20'>
+        <div className='flex items-center justify-between lg:px-20 md:px-14 sm:px-10 px-5'>
             <div>
-                <p className='lg:text-[48px] font-medium lg:leading-[64.8px] font-2'>Explore marketplace</p>
+                <p className='lg:text-[48px] md:text-[40px] sm:text-[30px] font-medium leading-[64.8px] font-2'>Explore marketplace</p>
             </div>
             <div>
-                <Link to="/market"><i class="bi bi-arrow-right-short text-[#4693ED] text-[81px]"></i></Link>
+                <Link to="/market"><i class="bi bi-arrow-right-short md:text-[#4693ED] md:text-[81px] sm:text-[40px] border rounded-full border-black sm:px-2 px-1 md:px-0 md:rounded-none md:border-none"></i></Link>
             </div>
         </div>
         <hr />
-        <div className='flex items-center justify-between lg:px-20'>
+        <div className='flex items-center justify-between lg:px-20 md:px-14 sm:px-10 px-5'>
             <div>
-                <p className='lg:text-[48px] font-medium lg:leading-[64.8px] font-2'>See auctions </p>
+                <p className='lg:text-[48px] md:text-[40px] sm:text-[30px] font-medium leading-[64.8px] font-2'>See auctions </p>
             </div>
-            <div>
-                <Link to="/auction"><i class="bi bi-arrow-right-short text-[#4693ED] text-[81px]"></i></Link>
+            <div className=''>
+                <Link to="/auction"><i class="bi bi-arrow-right-short md:text-[#4693ED] md:text-[81px] sm:text-[40px] border rounded-full border-black sm:px-2 px-1 md:px-0 md:rounded-none md:border-none"></i></Link>
             </div>
         </div>
         <hr />
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Explore from '../Componets/Explore'
 import FeaturedProduct from '../Componets/FeaturedProduct'
+import Footer from '../Componets/Footer'
 import HomeHero from '../Componets/Home-Hero'
 import Navbar from '../Componets/Navbar'
 import UpcomingAuction from '../Componets/UpcomingAuction'
@@ -13,6 +14,7 @@ const Home = () => {
       <FeaturedProduct/>
       <UpcomingAuction/>
       <Explore/>
+      <Footer/>
     </div>
   )
 }
